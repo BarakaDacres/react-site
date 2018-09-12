fca63a652724b52415eea1f56896f94b9121b361
Baraka Dacrés <barakadacres@Barakas-MBP.home>
2018-09-13 01:54:57 +1000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-	title: 'Design+Code 3',
+	title: 'Krew Real LLC',
 	description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
 	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: '0ge8xzmnbp2c',
-        accessToken: 'b3b275d4fda32085546b9100cb9ae7cb8796fdddd462fd42ba53c4c17bf2f99d'
+        spaceId: '9ffaquad83wg',
+        accessToken: '219e9e6479a63cb9c6a68a663ab0db3ba9068eea7abb3b5df359e161efd747dd'
       }
     }
   ],
